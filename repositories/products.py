65af54cd27a07484ab8db4b1d9562c0c6aff8b17
linwexEU <130746152+linwexEU@users.noreply.github.com>
2024-05-12 12:02:03 +0300

    Delete repositories directory

--- repositories/products.py
+++
@@ -1,6 +0,0 @@
-from models.products import Products
-from utils.repository import SQLAlchemyRepository
-
-
-class ProductsRepository(SQLAlchemyRepository):
-    model = Products
--- repositories/users.py
+++
@@ -1,6 +0,0 @@
-from models.users import Users
-from utils.repository import SQLAlchemyRepository
-
-
-class UsersRepository(SQLAlchemyRepository):
-    model = Users
